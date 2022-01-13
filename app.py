a79e9aa941db6657a7b60f3c24973fa8d905a921
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-13 17:30:47 +0100

    a

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate, upgrade
 from model import db, seedData
 from configDebug import ConfigDebug
 from personer.personpages import personBluePrint
+from model import User, user_manager
+from flask_user import login_required, roles_required, current_user
 
  
 app = Flask(__name__)
@@ -12,7 +14,8 @@ app.config.from_object(__name__ + '.ConfigDebug')
 db.app = app
 db.init_app(app)
 migrate = Migrate(app,db)
-
+user_manager.app = app
+user_manager.init_app(app,db,User)
 
 app.register_blueprint(personBluePrint)
 
@@ -21,6 +24,16 @@ def index() -> str:
     return render_template('index.html')
 
 
+@app.route('/tjena')
+@login_required
+def index3() -> str:
+    return "Tjena" + current_user.email
+
+
+@app.route('/secret')
+@roles_required("Admin")
+def index2() -> str:
+    return "secret"
 
 
 
